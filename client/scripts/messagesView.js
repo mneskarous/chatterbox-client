70792636d2231b738b7b4de1b7858134c11de7cb
mneskarous <mneskarous@gmail.com>
2019-07-26 15:39:16 -0700

    Completed POST request and added event listener to submit button

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,7 +3,14 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
-  },
+    $(document).ready(function() {
+    var userMessage = $('#message').val();
+    $('.submit').on('click', function(event) {
+      //alert('clicked');
+       $('#chats').css('font-size', '100px');
+    });
+  })
+},
 
   render: function() {
   }
