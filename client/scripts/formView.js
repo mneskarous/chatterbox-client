da95a24ba79e6c79fe94ebd7b4b5b5303f82925e
mneskarous <mneskarous@gmail.com>
2019-07-26 18:42:56 -0700

    Built a prototype for prepending to the page

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -1,6 +1,8 @@
 var FormView = {
 
   $form: $('form'),
+  // $userMessage: $('#message').val(),
+  $chats: $('#chats'),
 
   initialize: function() {
     FormView.$form.on('submit', FormView.handleSubmit);
@@ -9,6 +11,10 @@ var FormView = {
   handleSubmit: function(event) {
     // Stop the browser from submitting the form
     event.preventDefault();
+    var userMessage =  $('#message').val();
+    var textBox = $('<div></div>');
+    $(userMessage).prependTo(textBox);
+    (textBox).prependTo(FormView.$chats);
     
     console.log('click!');
   },
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,16 +3,32 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
-    $(document).ready(function() {
-    var userMessage = $('#message').val();
-    $('.submit').on('click', function(event) {
-      //alert('clicked');
-       $('#chats').css('font-size', '100px');
-    });
-  })
+  //   $(document).ready(function() {
+  //   $('.submit').on('click', function(event) {
+  //     //alert('clicked');
+  //      $(MessagesView.$userMessage).prependTo(MessagesView.$chats); // Work on this later
+  //   });
+  // })
 },
 
   render: function() {
+    $(document).ready(function() {
+
+      Parse.readAll(function(data) {
+        for (i = 0; i < data.results.length; i++) {
+          var obj = data.results[i];
+          var compiled = _.template(
+            ` <div class="username">Username: <%= obj.username %> </div>
+              <div class="room">Room: <%= obj.roomname %> <div>
+              <div class="text">Message: <%= obj.text %> </div>
+              <div class="timestamp">Posted at: <%= obj.createdAt %> </div>
+            `
+          );
+          $(compiled).prependTo($('#chats'));
+        }
+      });
+    });
+  
   }
 
 };
