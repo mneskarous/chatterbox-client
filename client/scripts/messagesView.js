137d4e2f85e6eed13b29bd99421dd2a91785e76d
Michael Eskarous <mneskarous@gmail.com>
2019-07-28 22:41:13 -0700

    Implemented initialize and render methods and refactored renderMessage method in messagesView

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,56 +3,21 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
-
-
+    MessagesView.$chats.on('click', '.username', MessagesView.handleClick);
   },
 
-  renderMessage: function() {
-
-      // var compiled = _.template(
-      //    "<div class='chat'>" +
-      //    "<div class='username'>username: <%= username %></div>" +
-      //    "<div class='text'>text: <%= text %></div>" +
-      //    "<div class='roomname'>roomname: <%= roomname %></div>" +
-      //    "</div>"
-      //   );
-      //  var html = "";
-      //  html += compiled(message);
-      //  (MessagesView.$chats).append(html);
-      // Messages.item()
-      // .filter(Room.isSelected(message))
-      // _.each(function(message) {
-      //   message['username'] = message['username'] || '';
-      //   message['text'] = message['text'] || '';
-      //   message['roomname'] = message['roomname'] || '';
-      //   var $message = MessageView.renderMessage(message);
-      //   MessagesView.$chats.append($message);
-      // });
-
-      // Messages
-      // .items()
-      // .filter(Room.isSelected(message))
-      // .each((message) => {
-      //   var $message = MessageView.renderMessage(message);
-      //   MessagesView.$chats.append($message);
-      // });
+  render: function() {
     Messages
       .items()
       .filter(message => Rooms.isSelected(message.roomname))
-      .each((message) => {
-        var $message = MessageView.renderMessage(message);
-        MessagesView.$chats.append($message);
-      });
+      .each(message => MessagesView.renderMessage(message));
+  },
+
+  renderMessage: function() {
+    var $message = MessageView.render(message);
+    MessagesView.$chats.prepend($message);
   }
+
 };
 
 
-
-
-
-
-
-        // ` <div class="username">Username: <%= username %> </div>
-        //   <div class="room">Room: <%= roomname %> </div>
-        //   <div class="text">Message: <%= text %> </div>
-        // `
